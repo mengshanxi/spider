08a2c733b9e68a4b40fb76f7f15335c55afe35ce
mengshanxi <13811668973@163.com>
2019-08-05 18:20:10 +0800

    开发

--- src/config/mylog.py
+++ src/config/mylog.py
@@ -1,26 +1,37 @@
 # -*- coding: utf-8 -*-
 import logging
 import logging.config
-from os import path
+from logging.handlers import TimedRotatingFileHandler
+import os
+import time
 
 # logging初始化工作
-
-log_file_path = path.join(path.dirname(path.abspath(__file__)), 'server.log')
-logging.basicConfig(level=logging.INFO,
-                    filemode='a',
-                    datefmt='%a, %d %b %Y %H:%M:%S',
-                    filename='server.log')
+logging.basicConfig(level=logging.INFO)
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = curPath[:curPath.find("spider") + len("spider")]
 
 
 def get_logger():
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
-    console.setFormatter(formatter)
-    logger = logging.getLogger('')
-    logger.addHandler(console)
+    loggers = logging.getLogger("Log")
+    console_log = logging.StreamHandler()
+    console_log.setLevel(logging.DEBUG)
+    data_format = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+    print(rootPath)
+    log_file_path = rootPath + os.sep + "src" + os.sep + "logs" + os.sep + "spider_" + data_format + '.log'
+    file_log = TimedRotatingFileHandler(log_file_path,
+                                        when="S",
+                                        interval=10,
+                                        backupCount=4)
+    file_log.setLevel(logging.WARNING)
 
-    return logger
+    console_fmt = logging.Formatter('%(asctime)s -%(name)s- %(levelname)s :%(message)s')
+    file_fmt = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
+    console_log.setFormatter(console_fmt)
+    file_log.setFormatter(file_fmt)
+
+    loggers.addHandler(console_log)
+    loggers.addHandler(file_log)
+    return loggers
 
 
 logger = get_logger()
--- src/test/test_website_dao.py
+++ src/test/test_website_dao.py
@@ -3,9 +3,10 @@ from manager.gather_center import GatherCenter
 from service.monitor_bc_service import MonitorBcService
 from service.weburl_service import WeburlService
 
-
+from config.mylog import logger
 class TestMysql(object):
     if __name__ == "__main__":
+        logger.info("gather task start!  batch_num:%s" % '11')
         # test='/usr/local/snapshots'
         # print(test[:10])
         # website_dao = WebsiteDao()
@@ -15,13 +16,14 @@ class TestMysql(object):
         # spider_manager.gather('', '')
         # weburl = WeburlService()
         # weburl.gather_urls(website)
-        service = MonitorBcService()
-        batch_num = '1'
-        # url = service.get_merchant_url('1', merchant_name='威海紫光科技园有限公司')
-        url = '/firm_6d1f03aac2ffea7e98592821fe618a62.html'
-        if url is not None:
-            try:
-                service.inspect(str(batch_num), "https://www.qichacha.com" + url, '威海紫光科技园有限公司','')
-                print()
-            except Exception as e:
-                pass
+        # service = MonitorBcService()
+        # batch_num = '1'
+        # # url = service.get_merchant_url('1', merchant_name='威海紫光科技园有限公司')
+        # url = '/firm_6d1f03aac2ffea7e98592821fe618a62.html'
+        # if url is not None:
+        #     try:
+        #         service.inspect(str(batch_num), "https://www.qichacha.com" + url, '威海紫光科技园有限公司','')
+        #         print()
+        #     except Exception as e:
+        #         pass
+
