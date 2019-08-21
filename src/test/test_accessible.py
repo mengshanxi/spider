e2e3d29db38ca3815ee3cfd1ddd99c301f0fee07
mengshanxi <13811668973@163.com>
2019-08-21 20:48:22 +0800

    优化与重构

--- src/test/test_accessible.py
+++ src/test/test_accessible.py
@@ -1,16 +1,18 @@
-from model.models import Weburl
-from service.accessible_service import AccessibleService
-from service.monitor_bc_service import MonitorBcService
-from service.monitor_weburl_service import MonitorWeburlService
+from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
 
 
 class TestEs(object):
     if __name__ == "__main__":
-        service = MonitorBcService()
-        url = service.get_merchant_url(str(1), merchant_name='河北纷橙电子商务有限公司')
-        if url is not None:
-            try:
-                service.inspect(str(1), "https://www.qichacha.com" + url, '河北纷橙电子商务有限公司',
-                                '靳建通')
-            except Exception as e:
-                pass
+        desired_capabilities = DesiredCapabilities.PHANTOMJS.copy()
+        headers = {
+            'cookie': ''}
+        for key, value in headers.items():
+            desired_capabilities['phantomjs.page.customHeaders.{}'.format(key)] = value
+        driver = webdriver.PhantomJS(executable_path='/agent/phantomjs',desired_capabilities=DesiredCapabilities.PHANTOMJS)
+        driver.set_page_load_timeout(10)
+        driver.set_script_timeout(10)
+        driver.maximize_window()
+        driver.get("www.baidu.com")
+        driver.save_screenshot("/usr/local/snapshots/aa.png")
+        driver.quit()
