c8fe1d7be6eedb6fb9d2c64b6bf73b5dc04a9e04
songtao <tao_song@ruijie.com.cn>
2019-09-02 19:04:29 +0800

    测试物流单号查询

--- src/test/test_logis.py
+++ src/test/test_logis.py
@@ -7,16 +7,35 @@ from config.mylog import logger
 
 class TestMysql(object):
     if __name__ == "__main__":
-        url = "https://www.trackingmore.com/bestex-tracking/cn.html?number=70634105326416"
-        driver = webdriver.Remote(command_executor='http://172.17.161.230:8911/wd/hub',
-                                  desired_capabilities=DesiredCapabilities.CHROME)
+        url = "https://www.trackingmore.com/choose-cn-70634105326416.html?"
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        driver = webdriver.Chrome(chrome_options=chrome_options,
+                                  executable_path="C:/chromedriver_2.38/chromedriver.exe")
 
-        driver.set_page_load_timeout(10)
-        driver.set_script_timeout(10)
+        driver.set_page_load_timeout(30)
+        driver.set_script_timeout(30)
         driver.maximize_window()
     try:
         driver.get(url)
-        driver.save_screenshot("D:/bb.jpg")
+        source = driver.page_source
+        soup = BeautifulSoup(source, 'html.parser')
+        logis_list = soup.find_all(attrs={'class': 'ulliselect'})
+        if logis_list is not None and logis_list.__len__() > 0:
+            for logis in logis_list:
+                try:
+                    href = logis.get("href")
+                    name = logis.get_text()
+                    print(name)
+                    driver.set_page_load_timeout(20)
+                    driver.set_script_timeout(20)
+                    driver.maximize_window()
+                    driver.get("https:"+href)
+                    driver.save_screenshot("D:/" + name + ".jpg")
+                except Exception as e:
+                    logger.error(e)
+                    driver.execute_script('window.stop()')
+                    driver.save_screenshot("D:/" + "ddd" + ".jpg")
         driver.quit()
     except Exception as e:
         logger.error(e)
