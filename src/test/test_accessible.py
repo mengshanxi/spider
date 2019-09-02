3dc0ac2565ebe42cfd174cfca52b1940d4199ac5
mengshanxi <13811668973@163.com>
2019-09-02 23:27:41 +0800

    Merge branch 'master' of https://github.com/mengshanxi/spider
    # Conflicts:
    #	src/service/monitor_website_service.py

--- src/test/test_accessible.py
+++ src/test/test_accessible.py
@@ -1,3 +1,4 @@
+import time
 from PIL import Image
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
@@ -10,25 +11,42 @@ from service.monitor_weburl_service import MonitorWeburlService
 from service.webdriver_util import WebDriver
 
 
-class TestEs(object):
-    if __name__ == "__main__":
-        # service = MonitorBcService()
-        # url = service.get_merchant_url(str(1), merchant_name='河北纷橙电子商务有限公司')
-        # if url is not None:
-        #     try:
-        #         service.inspect(str(1), "https://www.qichacha.com" + url, '河北纷橙电子商务有限公司',
-        #                         '靳建通')
-        #     except Exception as e:
-        #         pass
-        driver = webdriver.Remote(command_executor='http://172.17.161.230:8911/wd/hub',
-                                  desired_capabilities=DesiredCapabilities.CHROME)
+def take_screenshot(browser):
+    browser.set_window_size(1200, 900)
+    # 以下代码是将浏览器页面拉到最下面。
+    browser.execute_script("""
+        (function () {
+            var y = 0;
+            var step = 100;
+            window.scroll(0, 0);
+            function f() {
+                if (y < document.body.scrollHeight) {
+                    y += step;
+                    window.scroll(0, y);
+                    setTimeout(f, 100);
+                } else {
+                    window.scroll(0, 0);
+                    document.title += "scroll-done";
+                }
+            }
+            setTimeout(f, 1000);
+        })();
+    """)
+    time.sleep(1)
 
-        driver.set_page_load_timeout(5)
-        driver.set_script_timeout(5)
-        driver.maximize_window()
+
+if __name__ == "__main__":
     try:
-        driver.get("http://www.aa.com")
-        driver.save_screenshot("D:/bb.jpg")
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        driver = webdriver.Chrome(chrome_options=chrome_options,
+                                  executable_path="C:/chromedriver_2.38/chromedriver.exe")
+        driver.get('https://www.trackingmore.com/bestex-tracking/cn.html?number=70634105326416')
+        driver.set_page_load_timeout(30)
+        driver.set_script_timeout(30)
+        driver.maximize_window()
+        take_screenshot(driver)
+        driver.save_screenshot('1' + '.png')
         driver.quit()
     except Exception as e:
-        print(e)
+        driver.quit()
--- src/test/test_logis.py
+++ src/test/test_logis.py
@@ -1,6 +1,7 @@
+import time
 from bs4 import BeautifulSoup
-from selenium.webdriver import DesiredCapabilities
 from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
 
 from config.mylog import logger
 
@@ -8,34 +9,37 @@ from config.mylog import logger
 class TestMysql(object):
     if __name__ == "__main__":
         url = "https://www.trackingmore.com/choose-cn-70634105326416.html?"
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless')
-        driver = webdriver.Chrome(chrome_options=chrome_options,
-                                  executable_path="C:/chromedriver_2.38/chromedriver.exe")
+        driver = webdriver.Remote(command_executor='http://172.17.161.230:8911/wd/hub',
+                                  desired_capabilities=DesiredCapabilities.CHROME)
 
-        driver.set_page_load_timeout(30)
-        driver.set_script_timeout(30)
+        driver.set_page_load_timeout(10)
+        driver.set_script_timeout(10)
         driver.maximize_window()
     try:
         driver.get(url)
         source = driver.page_source
         soup = BeautifulSoup(source, 'html.parser')
         logis_list = soup.find_all(attrs={'class': 'ulliselect'})
+        driver.quit()
         if logis_list is not None and logis_list.__len__() > 0:
             for logis in logis_list:
+                href = logis.get("href")
+                name = logis.get_text()
+                print(name)
+                driver = webdriver.Remote(command_executor='http://172.17.161.230:8912/wd/hub',
+                                          desired_capabilities=DesiredCapabilities.CHROME)
+                driver.implicitly_wait(60)
+                driver.maximize_window()
                 try:
-                    href = logis.get("href")
-                    name = logis.get_text()
-                    print(name)
-                    driver.set_page_load_timeout(20)
-                    driver.set_script_timeout(20)
-                    driver.maximize_window()
-                    driver.get("https:"+href)
+                    print(href)
+                    url = "https:"+href
+                    driver.get(url)
                     driver.save_screenshot("D:/" + name + ".jpg")
                 except Exception as e:
                     logger.error(e)
                     driver.execute_script('window.stop()')
-                    driver.save_screenshot("D:/" + "ddd" + ".jpg")
-        driver.quit()
+                    driver.save_screenshot("D:/" + name + ".jpg")
+                    driver.quit()
     except Exception as e:
         logger.error(e)
+        driver.quit()
