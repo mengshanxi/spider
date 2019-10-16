5ab0ec552a87004f473fe89e78ddcb2c0b314653
songtao <tao_song@ruijie.com.cn>
2019-10-16 18:24:00 +0800

    功能优化

--- src/service/webdriver_util.py
+++ src/service/webdriver_util.py
@@ -50,6 +50,7 @@ class WebDriver:
     @staticmethod
     def get_chrome_by_local():
         chrome_options = Options()
+        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36")
         prefs = {"profile.managed_default_content_settings.images": 2, 'permissions.default.stylesheet': 2}
         chrome_options.add_experimental_option("prefs", prefs)
         driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',
