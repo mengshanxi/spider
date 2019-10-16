218fef7019224ef1070c58013cf6e54bbfd95919
mengshanxi <13811668973@163.com>
2019-10-16 21:23:57 +0800

    Merge branch 'develop' of https://github.com/mengshanxi/spider into develop
    # Conflicts:
    #	src/service/webdriver_util.py

--- src/test/test_tracking3.py
+++ src/test/test_tracking3.py
@@ -10,10 +10,6 @@ class TestMysql(object):
         chrome_options = Options()
         chrome_options.add_argument('--incognito')
         chrome_options.add_argument("--disk-cache-size=0")
-        chrome_options.add_argument('lang=zh_CN.UTF-8')
-        # 更换头部
-        chrome_options.add_argument(
-            'user-agent="Mozilla/5.0 (iPod; U; CPU iPhone OS 2_1 like Mac OS X; ja-jp) AppleWebKit/525.18.1 (KHTML, like Gecko) Version/3.1.1 Mobile/5F137 Safari/525.20"')
         driver = webdriver.Remote(command_executor='http://172.19.27.54:8912/wd/hub',
                                   desired_capabilities=DesiredCapabilities.CHROME, options=chrome_options)
         driver.set_page_load_timeout(60)
