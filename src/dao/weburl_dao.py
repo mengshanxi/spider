8e7e066d8ad9aedf814b3bfe348247f0fa1deb26
mengshanxi <13811668973@163.com>
2019-11-08 00:21:51 +0800

    优化url采集

--- src/dao/weburl_dao.py
+++ src/dao/weburl_dao.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 import datetime
 
-from dao.db import session
+from dao.db import session, DB_Session
 from model.models import Weburl
 
 
@@ -14,11 +14,18 @@ class WeburlDao(object):
 
     @staticmethod
     def add(weburl):
-        exist_weburl = session.query(Weburl).filter(Weburl.url == weburl.url).all()
-        if len(exist_weburl):
-            pass
-        else:
-            weburl.create_time = datetime.datetime.now()
-            weburl.last_update = datetime.datetime.now()
-            session.add(weburl)
-            session.commit()
+        conn = DB_Session()
+        try:
+            exist_weburl = conn.query(Weburl).filter(Weburl.url == weburl.url).all()
+            if len(exist_weburl):
+                pass
+            else:
+                weburl.create_time = datetime.datetime.now()
+                weburl.last_update = datetime.datetime.now()
+                conn.add(weburl)
+                session.commit()
+        except:
+            conn.rollback()
+            raise
+        finally:
+            conn.close()
